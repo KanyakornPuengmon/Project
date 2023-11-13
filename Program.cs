bcbef2de1018ee98b9cf54c760c43cd570c84e4e
KanyakornPuengmon <pear25466@gmail.com>
2023-11-13 14:27:57 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Project65030018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project members:");
+            foreach (string member in Enum.GetNames(typeof(Project.Members)))
+            {
+                Console.WriteLine("  [" +
+                    ((int)Enum.Parse(typeof(Project.Members), member)).ToString() + "]: "
+                    + member);
+            }
+
+            Console.WriteLine($"Project members count = {Enum.GetNames(typeof(Project.Members)).Length}");
+
+        }
+    }
+}
---
+++ Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project65030018
+{
+    public class Project
+    {
+        public enum Members
+        {
+            Pear = 1,
+            Ming = 2,
+            Mark = 3,
+            
+        }
+    }
+}
